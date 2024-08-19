56e67ce01797cb552251684ab29f51e332c8c77b
Roger Selzler <rogerselzler@carleton.ca>
2024-08-19 10:44:42 -0400

    fix in README and install

--- install.sh
+++ install.sh
@@ -5,23 +5,21 @@ ENVNAME="cenv"
 BLASTVERSION="2.16.0"
 BLASTFILE=ncbi-blast-$BLASTVERSION+-x64-linux.tar.gz
 
-
 echo "Downloading $BLASTFILE"
 wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/$BLASTVERSION/$BLASTFILE -O ~/$BLASTFILE
 tar zxvpf ~/$BLASTFILE -C ~
+rm -rf ~/$BLASTFILE
 
 export PATH=$PATH:$HOME/ncbi-blast-$BLASTVERSION+/bin
 PATHCMD="export PATH=\$PATH:$HOME/ncbi-blast-${BLASTVERSION}+/bin"
-grep -qxF "$PATHCMD" $HOME/.bashrc || echo $PATHCMD >> $HOME/.bashrc
+grep -qxF "$PATHCMD" $HOME/.bashrc || echo $PATHCMD >>$HOME/.bashrc
 
 [ -d $HOME/blastdb ] || mkdir $HOME/blastdb
 export BLASTDB=$HOME/blastdb
 BLASTDBCMD="export BLASTDB=\$HOME/blastdb"
-grep -qxF "$BLASTDBCMD" $HOME/.bashrc || echo $BLASTDBCMD >> $HOME/.bashrc
+grep -qxF "$BLASTDBCMD" $HOME/.bashrc || echo $BLASTDBCMD >>$HOME/.bashrc
 
-
-if ! command -v conda &> /dev/null
-then
+if ! command -v conda &>/dev/null; then
     echo "conda not found, installing miniconda"
     mkdir -p ~/miniconda3
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
@@ -42,12 +40,8 @@ echo "conda environment activated"
 echo "Installing required packages"
 conda config --add channels bioconda
 
-conda install -y pip
-conda install -y parsnp
+conda install -y pip parsnp
 
 pip install biopython pandas xlsxwriter
 
 echo "Installation complete"
-
-
-
